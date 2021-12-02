60c315c806ade3833c8d9a7804075b5ab9a22719
Lars Viding <lars.viding@coreit.se>
2021-12-02 09:27:19 +0100

    add .

---
+++ E4/AppInstallScripts/adjustment-of-unneeded-Apps.ps1
@@ -0,0 +1,38 @@
+Write-Output 'Start remove uneeded appx'
+
+#Remove uneeded appx
+$appname = @(
+"*windowscommunicationsapps*"
+"*officehub*"
+"*xbox*"
+"*music*"
+"*Solitaire*"
+"*mixed*"
+"*OneConnect*"
+"*BingWeather*"
+"*GetHelp*"
+"*Getstarted*"
+"*Messaging*"
+"*Print3D*"
+"*SkypeApp*"
+"*Microsoft.People*"
+"*Wallet*"
+"*Microsoft.Windows.Photos*"
+"*WindowsAlarms*"
+"*WindowsMaps*"
+"*YourPhone*"
+"*Zune*"
+"*WindowsSoundRecorder*"
+"*Microsoft3DViewer*"
+)
+ForEach($app in $appname){
+Get-AppxProvisionedPackage -Online | Where-Object {$_.PackageName -like $app} | Remove-AppxProvisionedPackage -Online -ErrorAction SilentlyContinue
+}
+Write-Output 'Finisched - Remove uneeded appx'
+#Disable Internet Explorer
+Write-Output 'Start Disable Internet Explorer'
+Disable-WindowsOptionalFeature -FeatureName Internet-Explorer-Optional-amd64 -Online -NoRestart
+Write-Output 'Finisched - Disable Internet Explorer'
+
+#Finish up
+Write-Output 'Finisched - adjustment of unneeded Apps'
