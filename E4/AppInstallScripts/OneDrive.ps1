cd76414e62201cf56e7b455011df7523b09a0547
Lars Viding <lars.viding@coreit.se>
2021-12-02 08:55:32 +0100

    add .

---
+++ E4/AppInstallScripts/OneDrive.ps1
@@ -0,0 +1,16 @@
+Write-Output 'OneDrive Start'
+$BuildDir = 'c:\CustomizerArtifacts'
+if (-not(Test-Path $BuildDir)) {
+    New-Item  -ItemType Directory $BuildDir
+}
+$allVersions = Find-EvergreenApp -Name MicrosoftOneDrive | Get-EvergreenApp
+$Ring = $allVersions | Where-Object { $_.Ring -eq 'Production'} | Sort-Object -Descending -Property 'Version'
+$myVersion = $Ring | Where-Object { $_.Architecture -eq 'AMD64'}
+$fileName = split-path $myVersion.uri -Leaf
+$outFile = join-path 'c:\CustomizerArtifacts' $fileName
+Invoke-WebRequest $myVersion.uri -OutFile $outFile
+$extPath = Join-Path  $BuildDir 'OneDrive'
+Start-Process -FilePath (Join-Path $extPath "\OneDriveSetup.exe") -ArgumentList '/allusers' -Wait
+Remove-Item $outFile
+Remove-Item $extPath -Recurse
+Write-Output 'OneDrive Installed'
