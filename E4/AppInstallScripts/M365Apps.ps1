2ec07c279d558b67c479dda630a4cd05178da73e
Lars Viding <lars.viding@coreit.se>
2022-11-13 17:22:20 +0100

    add ,

--- E4/AppInstallScripts/M365Apps.ps1
+++ E4/AppInstallScripts/M365Apps.ps1
@@ -4,6 +4,9 @@ $myVersion = $allVersions | Where-Object { $_.Channel -eq 'MonthlyEnterprise'}
 $fileName = split-path $myVersion.uri -Leaf
 $outFile = join-path 'c:\CustomizerArtifacts' $fileName
 Invoke-WebRequest $myVersion.uri -OutFile $outFile
+$XMLfileName = "https://raw.githubusercontent.com/LarsViding/AIB/main/E4/AppInstallScripts/annell-32bi-NO-teams-AVD.xml"
+$outXML = Join-Path 'c:\CustomizerArtifacts' $XMLfileName
+Invoke-WebRequest $myVersion.uri -OutFile $outFile
 
 C:\CustomizerArtifacts\Setup.exe /download Annell-32bit-AVD.xml
 C:\CustomizerArtifacts\Setup.exe /configure Annell-32bit-AVD.xml
