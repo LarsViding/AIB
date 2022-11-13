66dd4737dcb8ddbcd8441f1c73a62496a917c931
Lars Viding <lars.viding@coreit.se>
2022-11-13 17:40:38 +0100

    add .

--- E4/AppInstallScripts/M365Apps.ps1
+++ E4/AppInstallScripts/M365Apps.ps1
@@ -1,18 +1,27 @@
 # M365Apps
+Write-Output 'M365Apps Start'
+$BuildDir = 'c:\CustomizerArtifacts'
+if (-not(Test-Path $BuildDir)) {
+    New-Item  -ItemType Directory $BuildDir
+}
 $allVersions = Find-EvergreenApp -Name Microsoft365Apps | Get-EvergreenApp
 $myVersion = $allVersions | Where-Object { $_.Channel -eq 'MonthlyEnterprise'} 
 $fileName = split-path $myVersion.uri -Leaf
 $outFile = join-path 'c:\CustomizerArtifacts' $fileName
 Invoke-WebRequest $myVersion.uri -OutFile $outFile
-$XMLfileName = "https://raw.githubusercontent.com/LarsViding/AIB/main/E4/AppInstallScripts/annell-32bi-NO-teams-AVD.xml"
+
+$XMLfileURI = "https://raw.githubusercontent.com/LarsViding/AIB/main/E4/AppInstallScripts/Annell-32bit-NO-teams-AVD.xml"
+$XMLfileName = split-path $XMLfileURI -Leaf
 $outXML = Join-Path 'c:\CustomizerArtifacts' $XMLfileName
-Invoke-WebRequest $myVersion.uri -OutFile $outFile
+Invoke-WebRequest $XMLfileURI -OutFile $outXML
 
-C:\CustomizerArtifacts\Setup.exe /download Annell-32bit-AVD.xml
-C:\CustomizerArtifacts\Setup.exe /configure Annell-32bit-AVD.xml
+C:\CustomizerArtifacts\Setup.exe /download Annell-32bit-NO-teams-AVD.xml
+C:\CustomizerArtifacts\Setup.exe /configure Annell-32bit-NO-teams-AVD.xml
 
-$extPath = Join-Path  $BuildDir 'OneDrive'
-Start-Process -FilePath (Join-Path $extPath "\Setup.exe") -ArgumentList '/allusers' -Wait
+Start-Process -FilePath (Join-Path $BuildDir "\Setup.exe") /download Annell-32bit-NO-teams-AVD.xml -Wait
+Start-Process -FilePath (Join-Path $BuildDir "\Setup.exe") /configure Annell-32bit-NO-teams-AVD.xml -Wait
+
 Remove-Item $outFile
-Remove-Item $extPath -Recurse
-Write-Output 'OneDrive Installed'
+Remove-Item $outXML
+
+Write-Output 'M365Apps Installed'
